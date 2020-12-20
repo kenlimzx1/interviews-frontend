13067f9ffac05c8cfa770ee08dfa38fd753964e1
Stevan Leonardy <stevan11220@gmail.com>
2020-12-20 14:36:32 +0700

    add slide show

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './home.module.css';
+import Slideshow from './slideShow';
+import 'react-slideshow-image/dist/styles.css'
 
 const Home: React.FC = () => {
   return (
     <div className={styles.container}>
-      Home
+      <Slideshow/>
     </div>
   )
 }
---
+++ src/pages/home/slideShow/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import styles from './slideShow.module.css';
+import proto from '../../../assets/images/apple-proto.png';
+
+const slideImages = [
+  proto,
+  proto,
+  proto,
+];
+
+const slideProperties = {
+  scale: 0.4,
+  indicators: (i: number) => (<div className={styles.indicator}></div>)
+}
+
+const Slideshow: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <Slide easing="ease" {...slideProperties}>
+        <div className="each-slide">
+          <div className={styles.imageWrapper}>
+            <img src={slideImages[0]} alt="" />
+          </div>
+        </div>
+        <div className="each-slide">
+          <div className={styles.imageWrapper}>
+            <img src={slideImages[1]} alt="" />
+          </div>
+        </div>
+        <div className="each-slide">
+          <div className={styles.imageWrapper}>
+            <img src={slideImages[2]} alt="" />
+          </div>
+        </div>
+      </Slide>
+    </div>
+  )
+};
+
+export default Slideshow;
